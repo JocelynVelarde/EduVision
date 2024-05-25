367e15d035ba4dfa5526105bd31496a8aa07a17f
Diego Rossi <70494007+Diego785xd@users.noreply.github.com>
2024-05-25 13:40:30 -0600

    Update build_and_run.sh

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,24 +1,5 @@
-#!/bin/bash
-
-# Define variables
-IMAGE_NAME="combined_ultralytics_mediapipe"
-DISPLAY_VAR=":0"
-
 # Build the Docker image
-sudo docker build -t $IMAGE_NAME .
-
-# Export DISPLAY variable and allow connections
-export DISPLAY=$DISPLAY_VAR
-xhost +local:docker
-
-# Find the video devices
-VIDEO_DEVICES=$(ls /dev/video* | tr '\n' ' ')
-
+sudo docker build -t combined_image .
 
 # Run the Docker container
-sudo docker run -it --rm --net=host --runtime nvidia -- device 1 -e DISPLAY=$DISPLAY \
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v /tmp/argus_socket:/tmp/argus_socket \
-  --cap-add SYS_PTRACE \
-  $(for dev in $VIDEO_DEVICES; do echo --device $dev:$dev; done) \
-  $IMAGE_NAME /bin/bash -c 'GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/face_detection/face_detection_gpu --calculator_graph_config_file=mediapipe/graphs/face_detection/face_detection_mobile_gpu.pbtxt'
+sudo docker run -it --rm --net=host --runtime nvidia -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix -v /tmp/argus_socket:/tmp/argus_socket --cap-add SYS_PTRACE --device /dev/video0:/dev/video0 combined_image
