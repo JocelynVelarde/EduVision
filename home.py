be56ced8086cec26be2626c6ce11253f9b1d983b
JocelynVelarde <a01285780@itesm.mx>
2024-05-24 21:45:07 -0600

    add static html page for custom domain

--- home.py
+++ home.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import webbrowser
+
+url = 'https://www.streamlit.io/'
 
 st.title('Home')
+
+st.write('This is going to be our static website.')
+
+if st.button('Go to Panel'):
+    st.write('Redirecting to Panel...')
+    webbrowser.open_new_tab(url)
