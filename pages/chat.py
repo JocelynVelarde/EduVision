f09ded6096e0aeb6ccdd017f3e01d53a4f671efc
JocelynVelarde <a01285780@itesm.mx>
2024-05-26 02:05:04 -0600

    corrected function calling with ppt generator

--- pages/chat.py
+++ pages/chat.py
@@ -1,7 +1,9 @@
+import base64
 import streamlit as st
 from openai import OpenAI
 from algorithms.gpt4o.simple_text import ask_chat
 from algorithms.gpt4o.image_process import create_lesson_plan
+from algorithms.gpt4o.pptx_genai import generate_slide_titles, generate_slide_content, create_presentation
 
 st.set_page_config(
         page_title="EduVision",
@@ -53,3 +55,30 @@ with st.form("Study Plan Image Form"):
       st.warning("Generating study plan...")
       result = create_lesson_plan(input_image)
       st.write(result)
+
+def get_ppt_download_link(ppt_filename):
+    with open(ppt_filename, "rb") as file:
+        ppt_contents = file.read()
+    b64_ppt = base64.b64encode(ppt_contents).decode()
+    return f'<a href="data:application/vnd.openxmlformats-officedocument.presentationml.presentation;base64,{b64_ppt}" download="{ppt_filename}">Download the PowerPoint Presentation</a>'
+
+
+
+st.divider()
+st.subheader(':green[Type a content topic to start generating a PPT]')
+
+with st.form("Study Plan Text Form"):
+    topic = st.text_input("Enter the topic for your presentation:")
+    generate_button = st.form_submit_button("Generate Presentation")
+
+    if generate_button and topic:
+        with st.spinner("Generating presentation... Please wait."):
+            slide_titles = generate_slide_titles(topic)
+            slide_contents = [generate_slide_content(title) for title in slide_titles]
+            ppt_filename = create_presentation(topic, slide_titles, slide_contents)
+
+        st.success("Presentation generated successfully!")
+
+        # Provide a download link for the generated PowerPoint presentation
+        st.markdown(get_ppt_download_link(ppt_filename), unsafe_allow_html=True)
+
