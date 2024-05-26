535aa086fb980915b11589dc4723dffea371c955
JocelynVelarde <a01285780@itesm.mx>
2024-05-26 01:49:34 -0600

    stable connection with mongodb via lists and use of gpt3 to create pptx downloadable files

--- algorithms/mongodb_genai.py
+++ algorithms/mongodb_genai.py
@@ -2,16 +2,27 @@
 from pymongo.mongo_client import MongoClient
 import streamlit as st
 import urllib
+import pymongo
 
 password = st.secrets["MONGODB_ATLAS_CLUSTER_URI"]
 uri = "mongodb+srv://pollogunpao:" + urllib.parse.quote(password) + "@cluster0.ny8favk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = pymongo.MongoClient(uri)
 
-# Create a new client and connect to the server
-client = MongoClient(uri)
+db = client.lince_hack
+collection = db.educaation
+
+
+mydict = { "name": "John", "address": "Highway 37" }
+
 
 # Send a ping to confirm a successful connection
 try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
+    x = collection.insert_one(mydict)
+    
+
 except Exception as e:
     print(e)
+
+
+
+
